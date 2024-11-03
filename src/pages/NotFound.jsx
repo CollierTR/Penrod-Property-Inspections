8c4705548335d161b228efc726ed2fd9469caa40
CollierTR <80648892+CollierTR@users.noreply.github.com>
2024-11-02 22:15:36 -0500

    Built out the 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,8 +6,10 @@ export default function NotFound() {
   return (
     <div>
 
-      <div className="flex place-items-center justify-center h-screen  ">
-        <h1 className=" text-light text-center text-xl md:text-8xl">Page not found...</h1>
+      <div className="flex flex-col place-items-center gap-10 justify-center h-screen">
+        <p className="text-8xl text-light">Oops!</p>
+        <p className=" text-light text-center text-xl md:text-6xl">It looks like you&apos;re lost!</p>
+        <p className="text-[#074ab5] text-lg underline"><a href="/">return to home page</a></p>
       </div>
 
       <Footer></Footer>
